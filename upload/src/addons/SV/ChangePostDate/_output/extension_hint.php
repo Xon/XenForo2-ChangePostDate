c27a4dc7af3a8b654acc0317da5346e6dfba570a
yugensoft <contact@yugensoft.com>
2018-01-15 15:53:43 +0700

    Removed superfluous classes

--- upload/src/addons/SV/ChangePostDate/_output/extension_hint.php
+++ upload/src/addons/SV/ChangePostDate/_output/extension_hint.php
@@ -6,20 +6,9 @@
 namespace SV\ChangePostDate\XF\Entity
 {
 	class XFCP_Post extends \XF\Entity\Post {}
-	class XFCP_Thread extends \XF\Entity\Thread {}
 }
 
 namespace SV\ChangePostDate\XF\InlineMod
 {
 	class XFCP_Post extends \XF\InlineMod\Post {}
-}
-
-namespace SV\ChangePostDate\XF\Repository\InlineMod
-{
-	class XFCP_Post extends \XF\Repository\InlineMod\Post {}
-}
-
-namespace SV\ChangePostDate\XF\Repository
-{
-	class XFCP_Post extends \XF\Repository\Post {}
 }
